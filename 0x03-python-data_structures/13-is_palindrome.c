36a203f1fac99744618987579a22ba740353bf2b
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-09-08 23:06:54 +0100

    initial

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * is_palindrome - test list palindrom
+ * @head: pointer to list to be tested
+ * Return: int
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *a, *b, *h;
+
+	a = *head;
+	b = *head;
+	if (a == NULL || (*a).next == NULL)
+	{
+		return (1);
+	}
+	while ((*b).next)
+	{
+		b = (*b).next;
+	}
+	while ((*a).n == (*b).n)
+	{
+		if ((*a).next == b)
+		{
+			return (1);
+		}
+		a = (*a).next;
+		h = b;
+		b = *head;
+		while ((*b).next != h)
+		{
+			b = (*b).next;
+		}
+	}
+	return (0);
+}
