90f22c962e472637b67ee6a7eaf4fce4fa7fbb91
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-09-08 21:39:14 +0100

    fix counter

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,5 +5,5 @@ def no_c(my_string):
     for x in my_string:
         if my_string[x] == 'c' or my_string[x] == 'C':
             new_str = new_str[:(x - i)] + my_string[(x + 1):]
-            i++
+            i += 1
     return new_str
