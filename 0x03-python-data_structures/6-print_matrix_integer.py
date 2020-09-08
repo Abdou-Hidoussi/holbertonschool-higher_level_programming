e11d4a57d9642cd0470d207e1c7b7be9c1318cd0
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-09-08 22:15:57 +0100

    restructure

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for x in matrix:
-        print(*x, sep=" ")
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[i][j]), end='')
+        print()
