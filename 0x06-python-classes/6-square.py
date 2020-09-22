5a47d4fe1c602edf6377efcf852ecc508a2b683a
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-09-22 19:16:05 +0000

    fix int error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,8 +48,8 @@ class Square:
     @position.setter
     def position(self, value):
         if len(value) != 2 or not isinstance(value, tuple) or\
-           not isinstance(value[0], tuple) or value[0] < 0 or\
-           not isinstance(value[1], tuple) or value[1] < 0:
+           not isinstance(value[0], int) or value[0] < 0 or\
+           not isinstance(value[1], int) or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
