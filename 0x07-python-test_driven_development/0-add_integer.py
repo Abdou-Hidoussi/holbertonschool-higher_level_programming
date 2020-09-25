2e8588d03196f9613642aedf0321ed60f8e2c065
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-09-25 17:01:15 +0000

    added doc

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    """ Simple number additions """
     if not isinstance(a, (float, int)):
         raise TypeError("a must be an integer")
         return
