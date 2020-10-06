ee7fe2dbe7d245723cb7a266b56937c3a82147a5
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-10-06 19:05:33 +0000

    fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,4 +10,4 @@ def inherits_from(obj, a_class):
     a_class: Class
     return: Bool
     """
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) is not a_class
