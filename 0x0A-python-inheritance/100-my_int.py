9683013a17d20bb9552cf1e144d53aea374862b1
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-10-06 21:28:55 +0000

    initial

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+task 12
+"""
+class MyInt(int):
+    """ My inherit class """
+    def __init__(self, value):
+        self.value = value
+
+    def __ne__(self, comp):
+        return (self.value == comp)
+
+    def __eq__(self, comp):
+        return self.value != comp
