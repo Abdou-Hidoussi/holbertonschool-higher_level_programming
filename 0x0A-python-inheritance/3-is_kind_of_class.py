3ed2d5d8b1e87f37795a01a882bbb11318fcbf6f
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-10-06 18:59:26 +0000

    initial

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Task 3
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    obj: Object
+    a_class: Class
+    return: Bool
+    """
+    return isinstance(obj, a_class)
