a4efdc027264bac338358c535adde8b3087d0400
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-10-07 19:09:59 +0000

    initial

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -6,5 +6,5 @@ task 3
 
 def write_file(filename="", text=""):
     """write in file function"""
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
