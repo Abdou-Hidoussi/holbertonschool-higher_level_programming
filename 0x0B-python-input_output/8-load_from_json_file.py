ee0b4abd19bbf62ec05e335fe662ee2722bc504f
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-10-07 19:24:26 +0000

    initial

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+task 8
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """jason convert object function"""
+    with open(filename, encoding="utf-8") as f:
+        json.load(f)
