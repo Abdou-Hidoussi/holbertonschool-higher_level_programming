82f68682a2f514ff29d1820a263b1da0caa472e7
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-10-07 19:12:35 +0000

    initial

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -4,7 +4,7 @@ task 4
 """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """append file function"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
