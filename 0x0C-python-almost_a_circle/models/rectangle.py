472c3d85f1b03ebe5a51e8cb519542f1fc253f95
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-10-13 14:47:54 +0000

    display fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,10 +11,10 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """init function"""
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
@@ -83,10 +83,10 @@ class Rectangle(Base):
         else:
             for i in range(self.__x):
                 print("")
-            for j in range(0, self.__width):
+            for j in range(0, self.__height):
                 for k in range(self.__y):
                     print(" ", end='')
-                for l in range(self.__height):
+                for l in range(self.__width):
                     print("#", end='')
                 print("")
 
