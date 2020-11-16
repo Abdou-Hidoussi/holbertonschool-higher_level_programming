5772f2603d697654c0e1a4edcea22afae720b4cc
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-11-16 20:08:17 +0000

    fix

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- holberton
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score DESC;
--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- holberton
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
