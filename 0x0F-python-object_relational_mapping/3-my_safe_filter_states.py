f4ac032bd9c704366849144977856323db335391
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-12-12 18:50:03 +0000

    initial

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Task 3 """
+import MySQLdb
+from sys import argv
+
+
+def Task3():
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    point = db.cursor()
+    point.execute("SELECT * from states WHERE name LIKE '{}'\
+            COLLATE latin1_general_cs\
+            ORDER BY states.id", (argv[4]))
+    for state in point.fetchall():
+        print(state)
+    point.close()
+    db.close()
+
+if __name__ == "__main__":
+    Task3()
