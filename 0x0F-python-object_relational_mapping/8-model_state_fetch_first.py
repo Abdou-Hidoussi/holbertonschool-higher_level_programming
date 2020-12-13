774843e21b098db1b5c74ce6b492800e189445bd
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-12-13 20:12:23 +0000

    initial

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,7 +14,7 @@ def Task8():
     Session = sessionmaker(bind=engine)
     session = Session()
     instance = session.query(State).order_by(asc(State.id)).first()
-    if not instance:
+    if instance is None:
         print(str(instance.id) + ': ' + instance.name)
     else:
         print("Nothing")
