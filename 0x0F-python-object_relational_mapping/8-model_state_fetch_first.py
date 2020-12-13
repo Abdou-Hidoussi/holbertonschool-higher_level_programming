c1c354ffc47391070be4e93a32a8c5e46d3d07af
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-12-13 20:11:19 +0000

    initial

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Task 8 """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+
+
+def Task8():
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                           argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    instance = session.query(State).order_by(asc(State.id)).first()
+    if not instance:
+        print(str(instance.id) + ': ' + instance.name)
+    else:
+        print("Nothing")
+    session.close()
+
+if __name__ == "__main__":
+    Task8()
