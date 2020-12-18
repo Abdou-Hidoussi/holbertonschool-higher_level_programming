774e09673d05336d666832cf3e2f2ebaa89e9ed0
abdou-hidoussi <hidoussiabdou5@gmail.com>
2020-12-18 20:43:30 +0000

    initial

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Task 14 """
+from sys import argv
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+
+
+def Task14():
+    """ Task 14 """
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for city, state in session.query(City, State)\
+                              .filter(City.state_id == State.id)\
+                              .order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
+
+if __name__ == '__main__':
+    Task14()
