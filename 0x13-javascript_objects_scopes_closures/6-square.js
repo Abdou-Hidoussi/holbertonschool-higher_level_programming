f867d31e164139f48ed08383a4f9156eb01a1cc3
abdou-hidoussi <hidoussiabdou5@gmail.com>
2021-02-08 17:53:06 +0000

    initial

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const Mraba3 = require('./5-square');
+class Square extends Mraba3 {
+  charPrint (ch) {
+    if (ch == undefined) {
+      ch = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log((ch).repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
