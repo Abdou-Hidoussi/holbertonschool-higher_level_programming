0ddc268306116b63ce22d6884eeddce5b16c36b1
abdou-hidoussi <hidoussiabdou5@gmail.com>
2021-02-08 17:49:15 +0000

    initial

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
     constructor (width, height) {
-      this.width = width;
-      this.height = height;
+        this.width = width;
+        this.height = height;
     }
   }
   module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor (width, height) {
+        if (width > 0 && height > 0) {
+            this.width = width;
+            this.height = height;
+        }
+    }
+    print () {
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
+    }
+    rotate () {
+        let x = 0;
+        x = this.width;
+        this.width = this.height;
+        this.height = x;
+      }
+      double () {
+        this.width *= 2;
+        this.height *= 2;
+      }
+  }
+  module.exports = Rectangle;
