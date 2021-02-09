4231cfa3a71a3c834d56fd0bc6e2f4604b26f177
abdou-hidoussi <hidoussiabdou5@gmail.com>
2021-02-09 19:32:07 +0000

    fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,11 +7,9 @@ request(process.argv[2], function (error, url, body) {
     const list = JSON.parse(body).results;
     let count = 0;
     for (const film of list) {
-        for (let ch of film.characters) {
-            if (ch.search('/18/') > 0) {
-                count++;
-            }
-        }
+      for (const ch of film.characters) {
+        if (ch.search('/18/') > 0) { count++; }
+      }
     }
     console.log(count);
   }
