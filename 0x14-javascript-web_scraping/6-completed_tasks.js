dcc9cc2b4340056f58e3d45fc7fd8c2b5f29a50e
abdou-hidoussi <hidoussiabdou5@gmail.com>
2021-02-09 20:07:31 +0000

    initial

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,14 +1,13 @@
-#!/usr/bin/node
 const request = require('request');
 request(process.argv[2], function (error, url, body) {
   if (error) {
     console.log(error);
   } else {
     const list = JSON.parse(body);
-    let count = {};
+    const count = {};
     for (const task of list) {
       if (task.completed === true) {
-        if (task.userId in count) { count[task.userId]++; } else { count[task.userId] = 1; } 
+        if (task.userId in count) { count[task.userId]++; } else { count[task.userId] = 1; }
       }
     }
     console.log(count);
